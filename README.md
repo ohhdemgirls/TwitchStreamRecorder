d21a856ceb9236d1382395b90d2eec68a3fa4e0f
gerarddp <gerarddiazperlas@gmail.com>
2014-03-16 13:17:54 +0100

    Add README
    Added main file

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-TwitchStreamRecord
-==================
+TwitchStreamRecorder
+====================
 
 A short PHP script to record twitch streams with livestreamer automatically
+
+Installation 
+============
+
+You need to have livestreamer installed, go here for installation instructions http://livestreamer.tanuki.se/en/latest/install.html
+
+You also need to modify the $folder var to the folder you want to save the recordings, make sure writing permissions are enabled.
+
+To automate the recording add something like the following to your crontab or similar:
+
+*/5 * * * * curl http://127.0.0.1/recordStream.php?channel=gerarddp
+
+This will check every 5 minutes if the stream is up and start recording
---
+++ recordStream.php
@@ -0,0 +1,51 @@
+<?php
+	$channel = $_REQUEST['channel'];
+	$folder = "/your/folder/";
+
+	exec("pidof /usr/bin/python", $out);
+
+	$rec = false;
+	foreach ($out as $proc) {
+		
+		exec("ps -fp ".$proc, $out2);
+		if (preg_match("!$channel!", $out2[1])) {
+			$rec = true;
+		}
+	}
+
+	if (!$rec) {
+		if (checkAvailable($channel, 0)) {
+			echo "Channel online ... ";
+			//record($channel, 1);
+			echo "Recording";
+		}
+		else {
+			echo "Channel offline";
+		}
+	}
+	else echo "Already recording";	
+
+	function checkAvailable($channel, $debug) {
+		$req = "https://api.twitch.tv/kraken/streams?channel=";
+		$req .= $channel;
+		if ($debug) echo "Waiting response <br>";
+		$json = file_get_contents($req);
+		if ($json) {
+			$data = json_decode($json,true);
+
+			foreach ($data["streams"] as $item) {
+				if ($debug) echo "Viewers: ".$item['viewers']."<br>";
+				return ($item['viewers'] > 0);
+			}
+		}
+		else {
+			if ($debug) echo "Error getting data";
+		}
+	}
+
+	function record($channel, $debug) {
+		$cmd = "livestreamer twitch.tv/" . $channel . " best -o ". $folder . date('dmHis', time()) . $channel . ".avi";
+		exec("($cmd) >/dev/null 2>/dev/null &");
+		if ($debug) echo $cmd;
+	}
+?>
